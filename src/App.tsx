fbaacfc92cdb421a01905e13a32664aafb003fcd
vershov <vershov@clink.tech>
2023-10-20 18:48:45 +0400

    add eslint prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-     react app
-    </div>
-  );
+  return <div className="App">react app</div>;
 }
 
 export default App;
